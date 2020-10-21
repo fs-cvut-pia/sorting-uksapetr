691c2af0bf4de4dc457bd4b9a4429204787a6d6b
uksapetr <71826600+uksapetr@users.noreply.github.com>
2020-10-21 22:13:32 +0200

    Update trideni.h

--- trideni.h
+++ trideni.h
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <ctime>
+#include <exception>
+#include <vector>
 
+nacti(nazev_vstupniho_souboru, jmena){
+}
+
+serad(jmena){
+  //bubble sort
+}
+
+zeptej_se_jestli_vypsat{
+}
+
+vypis(jmena){
+}
